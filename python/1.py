812da5f26bee0480138f5ddb58ebd8173f6ec5ac
Reyhane Bakhtiary <rbakhtiary33@gmail.com>
2025-12-20 11:44:33 -0800

    Update 1.py

--- python/1.py
+++ python/1.py
@@ -1,1 +1,6 @@
-
+# Write Python code to iterate through the first 10 numbers and,
+# in each iteration, print the sum of the current and previous number.
+j=0
+for i in range(0,10):
+    print('current number',i,'previous number',j,'sum =',i+j)
+    j=i
