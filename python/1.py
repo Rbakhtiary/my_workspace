576f91fb86367b4f5fc31c7a7ca9d539bf82655c
Reyhane Bakhtiary <rbakhtiary33@gmail.com>
2025-12-21 11:51:45 -0800

    Update 1.py

--- python/1.py
+++ python/1.py
@@ -11,3 +11,10 @@ print('given list is:',list)
 for number in list:
  if number % 5 == 0:
      print(number)
+=========================
+# Write a code to generates a complete multiplication table for numbers 1 through 10.
+for i in range(1,11):
+    for j in range(1,11):
+        c=i*j
+        print(c,end=' ')
+    print()
